a3367435dce42abe40c84092feb2d78f6ba6903e
bozrem <bozungremy@gmail.com>
2026-03-25 12:27:51 -0500

    Add CMake and hello world

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,10 @@
  * File: main.cpp
  * Purpose: Provide the main user interaction through a REPL loop to run in simulation and add to parser
  */
+
+#include "iostream"
+
+int main() {
+  std::cout << "Hello, world!\n";
+  return 0;
+}
